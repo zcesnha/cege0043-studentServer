3cc96e6578c9e2e660170a9c5640fecaabf58e73
zcesnha <nicholas.hay.17@ucl.ac.uk>
2019-03-10 13:40:25 -0400

    Addition of Server JavaScript

---
+++ studentServer.js
@@ -0,0 +1,13 @@
+// express is the server that forms part of the nodejs program
+var express = require('express');
+var path = require("path");
+var app = express(); 
+ // add an http server to serve files to the Edge browser
+ // due to certificate issues it rejects the https files if they are not
+ // directly called in a typed URL
+ var http = require('http');
+ var httpServer = http.createServer(app);
+ httpServer.listen(4480);
+ app.get('/',function (req,res) {
+ res.send("hello world from the HTTP server");
+ });
